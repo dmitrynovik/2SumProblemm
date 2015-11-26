781659c3ddc6abd9e2b1e453dc60fb0e00b4746b
dmitrynovik <DmitryNovik@levo.com.au>
2015-11-26 15:26:14 +1100

    Heap test added

---
+++ TwoSum/BinaryHeapTest.cs
@@ -0,0 +1,18 @@
+﻿using NUnit.Framework;
+
+namespace TwoSum
+{
+    [TestFixture]
+    public class BinaryHeapTest
+    {
+        [Test]
+        public void TestHeap()
+        {
+            var heap = new BinaryHeap<int> {9, 2, 6, 7, 8, 5, 4, 3, 1, 0 };
+            for (int i = 0; i < heap.Count; ++i)
+            {
+                Assert.AreEqual(i, heap.Remove());
+            }
+        }
+    }
+}
