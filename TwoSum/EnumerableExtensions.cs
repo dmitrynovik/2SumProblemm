6c72da428c422536da7f39ae8984e1ecc730c963
dmitrynovik <DmitryNovik@levo.com.au>
2015-11-26 14:49:07 +1100

    Enum Extensions

---
+++ TwoSum/EnumerableExtensions.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+
+namespace TwoSum
+{
+    public static class EnumerableExtensions
+    {
+        public static HashSet<T> ToHashSet<T>(this IEnumerable<T> items)
+        {
+            var ret = new HashSet<T>();
+            if (items != null)
+            {
+                foreach (var i in items)
+                {
+                    ret.Add(i);
+                }
+            }
+            return ret;
+        } 
+    }
+}
